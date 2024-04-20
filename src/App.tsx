a7b97efddc84ae3e575e48eb74f82bf390e30d15
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-04-20 07:24:44 +0200

    correct routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import Header from './components/layout/Header/Header.tsx';
+import Home from './components/layout/SharedLayout/SharedLayout.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CategoriesContainer from './components/Products/CategoriesContainer.tsx';
+import ProductsContainer from './components/Products/ProductsContainer.tsx';
 
 const App = () => {
  
   return (
-    <>
-    <Header/>
-    <CategoriesContainer/>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}>
+          <Route path="category" element={<CategoriesContainer />} />
+          <Route path='category/:categoryName' element={<ProductsContainer/>} /> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
+ )
+
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App.tsx'
-import CategoriesContainer from './components/Products/CategoriesContainer.tsx';
 import './index.css'
-import ProductsContainer from './components/Products/ProductsContainer.tsx';
 
 const rootElement = document.getElementById('root');
   ReactDOM.createRoot(rootElement!).render(
-    <BrowserRouter>
-      <React.StrictMode>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="category" element={<CategoriesContainer />} />
-          <Route path='category/:categoryName' element={<ProductsContainer/>} />
-        </Routes>
-      </React.StrictMode>
-    </BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
   );
