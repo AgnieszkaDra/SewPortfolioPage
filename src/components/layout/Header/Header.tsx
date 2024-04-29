f1a64a6f4cf0170aadf9d804251cc1adf3124341
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-04-29 13:42:44 +0200

    styled header with styled-components

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,8 +1,41 @@
 import { useState }  from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 import Navigation from '../../ui/Navigation/Navigation';
 import Carousel from '../../ui/Carousel/Carousel';
 import carousel from '../../../data/carousel';
-import styles from './Header.module.scss';
+import styled from 'styled-components';
+
+const HamburgerWrapper = styled.div<{ navbarOpen: boolean }>`
+  height: 26px;
+  width: 32px;
+  position: absolute;
+  top: 17px;
+  left: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  svg {
+    width:100%;
+    height: 100%;
+    z-index: 3;
+  }
+`;
+
+const HeaderWrapper = styled.header`
+  height: 100vh;
+  position: relative;
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+const NavigationWrapper = styled.div`
+  width: 100%;
+  height: 10vh;
+  position: relative;
+  display: flex;
+  z-index: 3;
+  background-color: transparent;
+`;
 
 
 const Header  = () => {
@@ -13,18 +46,18 @@ const Header  = () => {
   }
 
   return (
-    <header className={styles.header}>
-      <div className={styles.header__nav}>
-      <div className={`${styles.hamburger} ${navbarOpen ? styles.close : ''}`} onClick={handleToggle}>
-        <span className={`${styles.hamburger__line} ${styles.line1} ${navbarOpen ? styles.close : ''}`}></span>
-        <span className={`${styles.hamburger__line} ${styles.line2} ${navbarOpen ? styles.close : ''}`}></span>
-        <span className={`${styles.hamburger__line} ${styles.line3} ${navbarOpen ? styles.close : ''}`}></span>
-      </div> 
-      <Navigation isNavbarOpen={navbarOpen}></Navigation>
-      </div>
-        <Carousel images={carousel}></Carousel>
-    </header>
-    
+    <HeaderWrapper>
+      <NavigationWrapper>
+        <HamburgerWrapper 
+          navbarOpen={navbarOpen} 
+          onClick={handleToggle}
+        >
+          { navbarOpen ? <IoMdClose /> : <GiHamburgerMenu /> } 
+        </HamburgerWrapper>
+        <Navigation isNavbarOpen={navbarOpen}></Navigation>
+      </NavigationWrapper>
+      <Carousel images={carousel}></Carousel>
+    </HeaderWrapper>
   );
 };
 
