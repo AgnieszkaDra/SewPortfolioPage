10051b54b7face67449177055db021bd17de4165
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-06-10 12:23:29 +0200

    change posiftion of carousel

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,28 +1,21 @@
-import { useLocation } from 'react-router-dom';import Navigation from '../../ui/Navigation/Navigation';
-import Carousel from '../../ui/Carousel/Carousel';
-import carousel from '../../../data/carousel';
+// import { useLocation } from 'react-router-dom';
+import Navigation from '../../ui/Navigation/Navigation';
 import styled from 'styled-components';
 
-const HeaderWrapper = styled.header<{ isShowCarousel: boolean }>`
+const HeaderWrapper = styled.header`
   height: 10vh;
   position: relative;
   color: ${({ theme }) => theme.colors.text};
-  ${props =>
-    props.isShowCarousel && `
-    height: 100vh;
-    background-color: transparent;
-  `}
 `;
 
 const Header  = () => {
  
-  const location = useLocation();
-  const showCarousel = location.pathname === '/' || location.pathname === '/category';
+  // const location = useLocation();
+  // const showCarousel = location.pathname === '/' || location.pathname === '/category';
   // const showNavigation = location.pathname === '/' || location.pathname === '/category';
   return (
-    <HeaderWrapper isShowCarousel={showCarousel}>
+    <HeaderWrapper>
       <Navigation />
-      {showCarousel && <Carousel images={carousel} />}
     </HeaderWrapper>
   );
 };
--- src/components/layout/Main/Main.tsx
+++ src/components/layout/Main/Main.tsx
@@ -1,14 +1,27 @@
-
+// import { useLocation } from 'react-router-dom';
 import CategoriesContainer from "../../ui/Categories/CategoriesContainer";
+import Carousel from '../../ui/Carousel/Carousel';
+import carousel from '../../../data/carousel';
+import styled from 'styled-components';
 
+const CarouselWrapper = styled.div`
+  margin-top: -10vh;
+  height: 100vh;
+  width: 100%;
+  background-color: transparent;
+`;
 
 const Main  = () => {
+  // const location = useLocation();
+  // const showCarousel = location.pathname === '/' || location.pathname === '/category';
  
-    return (
+  return (
     <main>
+      <CarouselWrapper>
+        <Carousel images={carousel} />
+      </CarouselWrapper>
       <CategoriesContainer/>
     </main>
-    
   );
 };
 
